0e04b55ac4bf2780328b84fd16803d81b5c45b3b
Luiz Guilherme <48565178+luizera-36@users.noreply.github.com>
2020-09-30 09:18:20 -0300

    validação de registro

--- functions/registro.php
+++ functions/registro.php
@@ -1,3 +1,4 @@
+<meta charset="utf-8">
 <?php
 require("conecta-banco.php");
 $nome = $_POST["nome"];
@@ -11,6 +12,18 @@ $uf = $_POST["uf"];
 $tipo = $_POST['TipoConta'];
 
 if($tipo == 1){
+    //VERIFICA SE O EMAIL JÁ ESTÁ SENDO UTILIZADO...
+    $comandoSQL = "SELECT email_restaurante from tb_restaurante WHERE email_restaurante='$email'";
+    $resultado_email = mysqli_query($con, $comandoSQL);
+    $total = mysqli_num_rows($resultado_email);
+
+    if($total){
+        echo "<script>
+        alert('A conta que você tentou criar já existe...');
+        window.location.href='../pages/login.php';
+        </script>";
+    }
+    else{
     //REGISTRA O RESTAURANTE
     $comandoSQL = "INSERT INTO tb_restaurante(nm_restaurante, email_restaurante, senha_restaurante, cep_restaurante, rua_restaurante, bairro_restaurante, cidade_restaurante, uf_restaurante) VALUES";
     $comandoSQL .= "('$nome', '$email', '$senha', '$cep', '$rua', '$bairro', '$cidade','$uf')";
@@ -25,8 +38,21 @@ if($tipo == 1){
     $con->close();
 
     header("Location: ../pages/login.php");
+    }
 }
 else if($tipo == 2){
+    //VERIFICA SE O EMAIL JÁ ESTÁ SENDO UTILIZADO...
+    $comandoSQL = "SELECT email_distribuidor from tb_distribuidor WHERE email_distribuidor='$email'";
+    $resultado_email = mysqli_query($con, $comandoSQL);
+    $total = mysqli_num_rows($resultado_email);
+    
+    if($total){
+        echo "<script>
+        alert('A conta que você tentou criar já existe...');
+        window.location.href='../pages/login.php';
+        </script>";
+    }
+    else{
     //REGISTRA O DISTRIBUIDOR
     $comandoSQL = "INSERT INTO tb_distribuidor(nm_distribuidor, email_distribuidor, senha_distribuidor, cep_distribuidor, rua_distribuidor, bairro_distribuidor, cidade_distribuidor, uf_distribuidor) VALUES";
     $comandoSQL .= "('$nome', '$email', '$senha', '$cep', '$rua', '$bairro', '$cidade','$uf')";
@@ -39,6 +65,6 @@ else if($tipo == 2){
     $dir = "../img_distribuidores/".$codigo[0];
     mkdir($dir, 0777);
     $con->close();
-
     header("Location: ../pages/login.php");
+    }
 }
